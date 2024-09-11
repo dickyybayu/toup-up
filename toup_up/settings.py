04387ab0a32f43d19568f0f354e78209af776e81
dikbayy <dicky.bayusadewo@gmail.com>
2024-09-11 11:05:47 +0700

    update allowed host

--- toup_up/settings.py
+++ toup_up/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u@2c%nr#171-i_quw6!s48j&gu$$qfqgtkt2fjn52!$m7e_0xh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","dicky-bayu31-toupup.pbp.cs.ui.ac.id"]
 
 # Application definition
 
