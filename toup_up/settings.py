af86c0c5e4dae1c42bc491d9d16f1afc493bf92d
dikbayy <dicky.bayusadewo@gmail.com>
2024-09-25 03:04:04 +0700

    Update Tugas 4

--- toup_up/settings.py
+++ toup_up/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u@2c%nr#171-i_quw6!s48j&gu$$qfqgtkt2fjn52!$m7e_0xh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","dicky-bayu31-toupup.pbp.cs.ui.ac.id"]
 
